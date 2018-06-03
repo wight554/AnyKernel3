d0dc8d1e8dc371a10f9873f98cee8bce1e7daf57
Vol Zhdanov <wight554@gmail.com>
2018-06-03 14:42:53 +0000

    AnyKernel2: override power configs instead of replacing them
    * Improve dymanic zram
    * Run all modifications in custom script
    * Add dalvik.vm.heapminfree tunable
    * Cleanup .rc config
    * Thanks to franco

--- ramdisk/init.emeriss.sh
+++ ramdisk/init.emeriss.sh
@@ -1,23 +1,38 @@
-#!/vendor/bin/sh
+#!/system/bin/sh
+
+################################################################################
+# helper functions to allow Android init like script
+
+function write() {
+    echo -n $2 > $1
+}
+
+function copy() {
+    cat $1 > $2
+}
+
+# macro to write pids to system-background cpuset
+function writepid_sbg() {
+    until [ ! "$1" ]; do
+        echo -n $1 > /dev/cpuset/system-background/tasks;
+        shift;
+    done;
+}
+
+function writepid_top_app() {
+    until [ ! "$1" ]; do
+        echo -n $1 > /dev/cpuset/top-app/tasks;
+        shift;
+    done;
+}
+################################################################################
+
+################################################################################
+# Dynamic ZRAM (512MB for 3GB RAM and 1024MB for 2GB RAM)
 MemTotalStr=`cat /proc/meminfo | grep MemTotal`
 MemTotal=${MemTotalStr:16:8}
 
-# Read adj series and set adj threshold for PPR and ALMK.
-# This is required since adj values change from framework to framework.
-adj_series=`cat /sys/module/lowmemorykiller/parameters/adj`
-adj_1="${adj_series#*,}"
-set_almk_ppr_adj="${adj_1%%,*}"
-
-# PPR and ALMK should not act on HOME adj and below.
-# Normalized ADJ for HOME is 6. Hence multiply by 6
-# ADJ score represented as INT in LMK params, actual score can be in decimal
-# Hence add 6 considering a worst case of 0.9 conversion to INT (0.9*6).
-set_almk_ppr_adj=$(((set_almk_ppr_adj * 6) + 6))
-echo $set_almk_ppr_adj > /sys/module/lowmemorykiller/parameters/adj_max_shift
-echo $set_almk_ppr_adj > /sys/module/process_reclaim/parameters/min_score_adj
-
-# Dynamic ZRAM
-configure_zram()
+configure_zram(SWAPSIZE)
 {
 if [ -e /sys/block/zram0/disksize ] ; then
   swapoff /dev/block/zram0  > /dev/null 2>&1
@@ -28,20 +43,69 @@ if [ -e /sys/block/zram0/disksize ] ; then
 fi
 }
 
-#Set Low memory killer minfree parameters
-# 64 bit up to 2GB with use 14K, and above 2GB will use 18K
-#
-# Set ALMK parameters (usually above the highest minfree values)
-# 64 bit will have 81K
-#
-# Also dynamically set zram size (512MB for 3GB RAM and 1024MB for 2GB RAM)
 if [ $MemTotal -gt 2097152 ]; then
-    echo "18432,23040,27648,32256,55296,80640" > /sys/module/lowmemorykiller/parameters/minfree
-    SWAPSIZE=536870912
+    configure_zram(536870912)
 else
-    echo "14746,18432,22118,25805,40000,55000" > /sys/module/lowmemorykiller/parameters/minfree
-    SWAPSIZE=1073741824
+    configure_zram(1073741824)
 fi
+################################################################################
 
-# Run ZRAM configure
-configure_zram
+{
+
+sleep 10
+
+# cpu
+chmod 0664 /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+chmod 0664 /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+chmod 0664 /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+chmod 0664 /sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq
+chmod 0664 /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq
+chmod 0664 /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor
+
+# configure governor settings for little cluster
+write /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq 691200
+write /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq 1401600
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/above_hispeed_delay "0 691200:20000 806400:24000 1190400:38000"
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/fastlane 0
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/fastlane_threshold 50
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/go_hispeed_load 99
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/go_lowspeed_load 10
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/hispeed_freq 806400
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/io_is_busy 1
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/align_windows 0
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/target_loads "60 400000:62 691200:70 806400:76 1017600:81 1190400:86 1305600:89 1382400:91 1401600:94"
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/min_sample_time 45000
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/powersave_bias 1
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/timer_rate 35000
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/timer_rate_screenoff 50000
+
+# configure governor settings for big cluster
+write /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq 883200
+write /sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq 1804800
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/above_hispeed_delay "30000 1056000:10000 1305600:30000"
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/fastlane 0
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/fastlane_threshold 50
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/go_hispeed_load 99
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/go_lowspeed_load 10
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/hispeed_freq 1056000
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/io_is_busy 1
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/align_windows 0
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/target_loads "83 883200:84 998400:85 1113600:84 1190400:86 1305600:85 1612800:83 1804800:95"
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/min_sample_time 40000
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/powersave_bias 1
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/timer_rate 20000
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/timer_rate_screenoff 50000
+
+# Input boost configuration
+write /sys/module/cpu_boost/parameters/input_boost_freq "0:1190400"
+write /sys/module/cpu_boost/parameters/input_boost_ms 400
+
+# enable Audio High Performance Mode
+write /sys/module/snd_soc_msm8x16_wcd/parameters/high_perf_mode 1
+
+# Disable perfd
+stop perfd
+
+sleep 20
+
+}&

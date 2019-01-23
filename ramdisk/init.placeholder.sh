50b37c33225cbb711222db09c0c6f9398d30203c
Vol Zhdanov <wight554@gmail.com>
2019-01-23 03:01:51 +0000

    AnyKernel2: ramdisk: small housekeeping
    * remove unused stuff and stuff we can't override
    * move fs tune to custom script

--- ramdisk/init.placeholder.sh
+++ ramdisk/init.placeholder.sh
@@ -1,52 +1,32 @@
 #!/system/bin/sh
 
-################################################################################
-# helper functions to allow Android init like script
-
 function write() {
     echo -n $2 > $1
 }
 
-function copy() {
-    cat $1 > $2
-}
-
-# macro to write pids to system-background cpuset
-function writepid_sbg() {
-    until [ ! "$1" ]; do
-        echo -n $1 > /dev/cpuset/system-background/tasks;
-        shift;
-    done;
-}
-
-function writepid_top_app() {
-    until [ ! "$1" ]; do
-        echo -n $1 > /dev/cpuset/top-app/tasks;
-        shift;
-    done;
-}
-################################################################################
-
 {
+    sleep 10
 
-sleep 10
+    # end boot time fs tune
+    write /sys/block/sda/queue/read_ahead_kb 128
+    write /sys/block/sda/queue/nr_requests 128
+    write /sys/block/sda/queue/iostats 1
+    write /sys/block/sda/queue/scheduler cfq
+    write /sys/block/sde/queue/read_ahead_kb 128
+    write /sys/block/sde/queue/nr_requests 128
+    write /sys/block/sde/queue/iostats 1
+    write /sys/block/sde/queue/scheduler cfq
 
-# cpu
-chmod 0664 /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-chmod 0664 /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor
+    # configure governor settings
+    write /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor "schedutil"
+    write /sys/devices/system/cpu/cpu0/cpufreq/schedutil/down_rate_limit_us 20000
+    write /sys/devices/system/cpu/cpu0/cpufreq/schedutil/iowait_boost_enable 1
+    write /sys/devices/system/cpu/cpu0/cpufreq/schedutil/up_rate_limit_us 500
 
-write /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor "schedutil"
-write /sys/devices/system/cpu/cpu0/cpufreq/schedutil/up_rate_limit_us 500
-write /sys/devices/system/cpu/cpu0/cpufreq/schedutil/down_rate_limit_us 20000
-write /sys/devices/system/cpu/cpu0/cpufreq/schedutil/iowait_boost_enable 1
-write /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq 300000
+    write /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor "schedutil"
+    write /sys/devices/system/cpu/cpu4/cpufreq/schedutil/down_rate_limit_us 20000
+    write /sys/devices/system/cpu/cpu4/cpufreq/schedutil/iowait_boost_enable 1
+    write /sys/devices/system/cpu/cpu4/cpufreq/schedutil/up_rate_limit_us 500
 
-write /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor "schedutil"
-write /sys/devices/system/cpu/cpu4/cpufreq/schedutil/up_rate_limit_us 500
-write /sys/devices/system/cpu/cpu4/cpufreq/schedutil/down_rate_limit_us 20000
-write /sys/devices/system/cpu/cpu4/cpufreq/schedutil/iowait_boost_enable 1
-write /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq 300000
-
-sleep 20
-
+    sleep 20
 }&
